c5f917fa05d20017a95174a0cea71d0b2af6675d
lolichaus <pooltyzq@yandex.ru>
2023-04-04 17:39:32 -0700

    added second and third line

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 int main()
 {
-	std::cout << "Hello GM@" <<std::endl;
+	std::cout << "Hello GM!" << std::endl;
+	std::cout << "Second line" << std::endl;
+	std::cout << "Third line" << std::endl;
 	return 0;
 }
 
